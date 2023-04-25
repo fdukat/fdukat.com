59c5fdd0fe005c8028b53e5715598afc4e76fb6e
fdukat <contact@fdukat.com>
2023-04-25 16:40:36 +0200

    implement roboto as a primary font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,19 @@
 import type { AppProps } from 'next/app';
 
+import { Roboto } from 'next/font/google';
+
+import '../styles/reset.css';
+import '../styles/global.css';
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500'],
+  subsets: ['latin'],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
