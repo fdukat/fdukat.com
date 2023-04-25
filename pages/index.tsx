fbba474ec405be1288171b054432d18f4c0db26a
fdukat <contact@fdukat.com>
2023-04-25 21:29:26 +0200

    add container and hero components to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import Head from 'next/head';
+
+import Container from '@/components/Container';
+import Hero from '@/components/Hero';
+
 export default function HomePage() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | Filip Dukat</title>
+      </Head>
+      <Container>
+        <Hero />
+      </Container>
+    </>
+  );
 }
