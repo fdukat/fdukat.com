e52775c9c4ac8762383f809b468d0b33eb563dd0
fdukat <contact@fdukat.com>
2023-04-27 01:28:05 +0200

    add meta tags for the document

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import {
   Github,
   Linkedin,
-  Twitter,
   BookOpen,
   FileText,
 } from 'feather-icons-react/build';
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          name="description"
+          content="Hey, my name is Filip, I am a software developer and this is a place for my thoughts and opinions about programming. Welcome!"
+        />
+        <meta
+          name="keywords"
+          content="Filip, Dukat, Software Developer, Frontend, React, Next.js, Programming, Poland, Polska, programista, engineer, programmer, blog"
+        />
+        <meta name="author" content="Filip Dukat" />
         <link rel="icon" href="favicon.ico" />
       </Head>
       <body>
