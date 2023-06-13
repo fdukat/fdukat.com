243842bbc6b0588b4617e3f8c65b95aa9c1b47ee
fdukat <contact@fdukat.com>
2023-06-13 16:52:09 +0200

    set noto sans as a default font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import '@/styles/tailwind.css';
-import { Inter } from 'next/font/google';
+import { Noto_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const notoSans = Noto_Sans({
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+});
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={notoSans.className}>{children}</body>
     </html>
   );
 }
