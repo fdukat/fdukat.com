b416f4cd379a741cec95fea71c11f2a953fca7a1
fdukat <contact@fdukat.com>
2023-06-15 19:38:18 +0200

    change footer font weight

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer className="h-24 flex items-center justify-center mt-36">
-      <p className="text-secondary font-extralight text-base w-fit">
-        2023 &copy; Crafted with 💜 by <strong>Filip Dukat</strong>
+      <p className="text-secondary font-light text-base w-fit">
+        2023 &copy; Crafted with 💜 by Filip Dukat
       </p>
     </footer>
   );
