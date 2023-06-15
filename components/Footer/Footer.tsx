43af119dc22fc5911480dc3744d1bb1987ff0261
fdukat <contact@fdukat.com>
2023-06-15 15:31:19 +0200

    create footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="h-24 flex items-center justify-center mt-36">
+      <p className="text-secondary font-extralight text-base w-fit">
+        2023 &copy; Crafted with 💜 by <strong>Filip Dukat</strong>
+      </p>
+    </footer>
+  );
+}
