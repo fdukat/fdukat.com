996fad2ba1535eed7b048c26881faea8b618bc53
fdukat <contact@fdukat.com>
2023-06-19 02:14:06 +0200

    add ackee analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Noto_Sans } from 'next/font/google';
 import Footer from '@/components/Footer';
 import '@/styles/loadAnimation.css';
 import '@/styles/tailwind.css';
+import Script from 'next/script';
 
 const notoSans = Noto_Sans({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -16,6 +17,13 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Script
+        id="analytics"
+        async
+        src="https://fdukat-analytics.vercel.app/tracker.js"
+        data-ackee-server="https://fdukat-analytics.vercel.app"
+        data-ackee-domain-id="18994671-ea21-4f9d-bac1-73398cc82540"
+      />
       <body className={`${notoSans.className} bg-background min-h-screen relative`}>
         {children}
         <Footer />
